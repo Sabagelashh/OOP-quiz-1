a129681b717bafe851dc154137e35978d95dc126
Sabagelashh <147238067+Sabagelashh@users.noreply.github.com>
2024-03-11 11:57:41 +0400

    Add files via upload

---
+++ Assignment01.java
@@ -0,0 +1,9 @@
+package quizzes.q1.saba_gelashvili_2;
+
+public class Assignment01 {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 18;
+        System.out.println("The sum of the numbers is " + (a + b));
+    }
+}
---
+++ Assignment03.java
@@ -0,0 +1,8 @@
+package quizzes.q1.saba_gelashvili_2;
+
+public class Assignment03 {
+    public static void main(String[] args) {
+        // Manually found the path to hidden.txt and printed it below
+        System.out.println("scooter.piano.board/hidden.txt");
+    }
+}
